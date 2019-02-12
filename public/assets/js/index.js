17ed6e294fbaee829e385fabb394f99c5bdafdff
Vadym Pintia <vadym.pintia.ext@tourstream.eu>
2019-02-12 22:11:49 +0200

    Add displaying of book photos

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -3,29 +3,43 @@ const mainList = document.querySelector('#book-list');
 function addBookToList(data) {
     data.forEach(book => {
         const li = document.createElement('li');
+        li.id = 'book-' + book.id;
         li.className = 'list-group-item';
 
         const title = document.createElement('h5');
         const description = document.createElement('p');
         const authorInfo = document.createElement('p');
-        const editButton = document.createElement('button');
+        const editButton = document.createElement('a');
         const deleteButton = document.createElement('button');
+        let image = null;
 
         title.innerText = book.name;
         description.innerText = book.description;
         authorInfo.innerText = book.author.fullName;
 
-        editButton.className = 'btn btn-warning';
+        if (book.picturePath) {
+            image = document.createElement('img');
+            image.className = 'img-fluid';
+            image.src = book.picturePath;
+        }
+
+        editButton.className = 'edit-book btn btn-warning';
+        editButton.href = 'book-form.html?id=' + book.id;
         editButton.innerText = 'Edit';
         editButton.dataset.index = book.id;
 
-        deleteButton.className = 'btn btn-danger';
+        deleteButton.className = 'delete-book btn btn-danger';
         deleteButton.innerText = 'Delete';
         deleteButton.dataset.index = book.id;
 
         li.appendChild(title);
         li.appendChild(description);
         li.appendChild(authorInfo);
+
+        if (image) {
+            li.appendChild(image);
+        }
+
         li.appendChild(editButton);
         li.appendChild(deleteButton);
 
