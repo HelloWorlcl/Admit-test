8e1848c95e5c59ccb2ea4d0a1b118aad11cd2834
Vadym Pintia <vadym.pintia.ext@tourstream.eu>
2019-02-19 01:22:13 +0200

    Add simple service container

---
+++ src/Kernel/ServiceContainer/dependencies.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    \App\Controllers\BookController::class => [
+        \App\Repositories\BookRepository::class,
+        \App\Models\Factories\BookFactory::class
+    ],
+    \App\Controllers\AuthorController::class => [
+        \App\Repositories\AuthorRepository::class
+    ],
+    \App\Repositories\BookRepository::class => [
+        \App\Kernel\Database\Connection\MySQLConnection::class,
+        \App\Models\Factories\BookFactory::class
+    ],
+    \App\Repositories\AuthorRepository::class => [
+        \App\Kernel\Database\Connection\MySQLConnection::class,
+        \App\Models\Factories\AuthorFactory::class
+    ],
+    \App\Models\Factories\BookFactory::class => [
+        \App\Models\Factories\AuthorFactory::class
+    ]
+];
